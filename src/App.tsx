4bb4e24d261d95ac9f0c4ebc1989594f4aa0d4fd
Roman Matsuiev <romanmatsuiev@gmail.com>
2023-08-07 20:55:11 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material';
+import './App.css';
+
+function App() {
+
+  return (
+    <Box>
+      <p style={{ color: 'black' }}>APP</p>
+    </Box>
+  );
+}
+
+export default App;
